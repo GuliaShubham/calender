547095d33f15e78fe6870ac25d57d41ed317154e
Shubham Gulia <65462654+GuliaShubham@users.noreply.github.com>
2022-10-19 23:51:24 +0530

    Add files via upload

---
+++ src/styles/theme.js
@@ -0,0 +1,73 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  brand: {
+    100: '#F9FBFF',
+    200: '#EEF4FF',
+    300: '#DEE3F5',
+    350: '#d2daf2',
+    400: '#C3D0F9',
+    450: '#b0c1f2',
+    500: '#733D47',
+    600: '#1B1C1E',
+  },
+  navigation: {
+    300: '#F4F4F4',
+    600: '#414141',
+  },
+  hover: {
+    600: '#1B1C1E22',
+  },
+  myGray: {
+    500: '#3A3C40',
+  },
+  blue: {
+    100: '#7CAEF8',
+    200: '#4E8FF1',
+    300: '#2272EB',
+    400: '#005CE6',
+  },
+};
+
+const theme = extendTheme({
+  colors,
+  styles: {
+    global: {
+      body: {
+        fontFamily: 'Poppins',
+      },
+    },
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        _focus: { boxShadow: 'none' },
+        // _hover: {
+        //   bg: 'brand.400',
+        //   color: 'brand.600',
+        // },
+      },
+      variants: {
+        base: {
+          //   bg: 'brand.500',
+          color: 'brand.500',
+        },
+        navigation: {
+          bg: 'navigation.300',
+          color: 'navigation.600',
+        },
+      },
+      defaultProps: {
+        variant: 'base',
+      },
+    },
+    Tooltip: {
+      baseStyle: {
+        bg: 'brand.600',
+        fontSize: 'xs',
+      },
+    },
+  },
+});
+
+export default theme;
